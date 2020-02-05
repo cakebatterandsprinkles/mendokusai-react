b8ce55d27eec0a732f789b10b979fb8516022283
yagmurcetintas <yagmurcetin@gmail.com>
2020-02-05 12:33:05 -0500

    added poster logic to the UserMainPage component

--- src/containers/UserMainPage/UserMainPage.jsx
+++ src/containers/UserMainPage/UserMainPage.jsx
@@ -1,17 +1,58 @@
 import React, { Component } from "react";
 import Aux from "../../hoc/Aux";
 import classes from "../UserMainPage/UserMainPage.module.css";
+import CalendarButton from "../../assets/images/calendar.png";
+import Poster1 from "../../assets/images/poster11.png";
+import Poster2 from "../../assets/images/poster22.png";
+import Poster3 from "../../assets/images/poster33.png";
+import Poster4 from "../../assets/images/poster44.png";
 
 class UserMainPage extends Component {
+  state = {
+    chosenPoster: ""
+  };
+
+  changePosterRandom = () => {
+    const posterArray = [Poster1, Poster2, Poster3, Poster4];
+    const randomNum = Math.floor(Math.random() * 4);
+    let chosenPoster = posterArray[randomNum];
+    console.log("changePosterRandom() : " + chosenPoster);
+    return this.updateChosenPoster(chosenPoster);
+  };
+
+  updateChosenPoster = chosenPoster => {
+    this.setState({ chosenPoster: chosenPoster });
+    console.log("updateChosenPoster() : " + this.state.chosenPoster);
+  };
+
   render() {
+    if (!this.state.chosenPoster) {
+      this.changePosterRandom();
+    }
+
     return (
       <Aux>
         <div className={classes.flexContainer}>
-          <div className={classes.half}>
+          <div className={classes.wrapper}>
             <p>Greetings, summoner</p>
             <p>Today looks like this:</p>
           </div>
-          <div className={classes.half}></div>
+          <div className={classes.posterAndButtonContainer}>
+            <div>
+              <img
+                src={this.state.chosenPoster}
+                alt="poster"
+                className={classes.poster}
+              />
+            </div>
+            <div className={classes.buttonContainer}>
+              <img
+                src={CalendarButton}
+                alt="calendar button"
+                className={classes.calendarButton}
+              />
+            </div>
+          </div>
         </div>
       </Aux>
     );
