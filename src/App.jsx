ffc31e401e08d1347ecf991f6892a68aea1ba0bc
yagmurcetintas <yagmurcetin@gmail.com>
2020-02-06 12:30:34 -0500

    added basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import LandingLayout from "./layouts/LandingLayout";
 import UserMainPageLayout from "./layouts/UserMainPageLayout";
 
-function App() {
-  return (
-    <div className="App">
-      <UserMainPageLayout />
-    </div>
-  );
-}
+const App = () => (
+  <Router>
+    <Fragment>
+      <Switch>
+        <Route exact path="/" component={LandingLayout} />
+        <Route exact path="/user" component={UserMainPageLayout} />
+      </Switch>
+    </Fragment>
+  </Router>
+);
 
 export default App;
