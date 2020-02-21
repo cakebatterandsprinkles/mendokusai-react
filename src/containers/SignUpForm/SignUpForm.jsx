0505eb2d220b71360d09842cc5920bcc8d684d22
yagmurcetintas <yagmurcetin@gmail.com>
2020-02-21 12:11:28 -0500

    added media queries to the signup page

--- src/containers/SignUpForm/SignUpForm.jsx
+++ src/containers/SignUpForm/SignUpForm.jsx
@@ -15,8 +15,8 @@ class SignUpForm extends Component {
             <label htmlFor="name">Name:</label>
             <input type="name" name="name" id="name"></input>
           </div>
-          <div className={classes.subText}>
-            <p>
+          <div className={classes.subTextContainer}>
+            <p className={classes.subText}>
               Note: Calendar will call you with this name. Choose whatever name
               you like.
             </p>
@@ -27,8 +27,10 @@ class SignUpForm extends Component {
             <label htmlFor="email">Email:</label>
             <input type="email" name="email" id="email"></input>
           </div>
-          <div className={classes.subText}>
-            <p>Note: You will need to verify this. Make it real.</p>
+          <div className={classes.subTextContainer}>
+            <p className={classes.subText}>
+              Note: You will need to verify this. Make it real.
+            </p>
           </div>
           <div
             className={`${classes.formGroupContainer} ${classes.marginBottom}`}
@@ -58,12 +60,19 @@ class SignUpForm extends Component {
             <label htmlFor="city">City:</label>
             <input type="city" name="city" id="city"></input>
           </div>
-          <div>
-            <input type="checkbox" name="vehicle1" value="Bike" />
-            <label htmlFor="vehicle1">
-              {" "}
-              I dislike filling forms. Use telepathy next time.{" "}
-            </label>
+          <div className={classes.subTextContainer}>
+            <div className={classes.subText}>
+              <input
+                type="checkbox"
+                name="dislike"
+                value="justforfun"
+                className={classes.checkboxLabel}
+              />
+              <label htmlFor="dislike">
+                {" "}
+                I dislike filling forms. Use telepathy next time.{" "}
+              </label>
+            </div>
           </div>
           <input type="hidden" name="_csrf" value="csrfToken" />
           <div className={classes.btnWrapper}>
