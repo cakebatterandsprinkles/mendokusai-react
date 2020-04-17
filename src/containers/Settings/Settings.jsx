dc13ba661914b0e0310247cc92c46c4668925f2c
yagmurcetintas <yagmurcetin@gmail.com>
2020-04-17 10:31:31 -0400

    deleted the layouts folder and created a layout hoc instead

--- src/containers/Settings/Settings.jsx
+++ src/containers/Settings/Settings.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import classes from "./SignUpForm.module.css";
+import classes from "./Settings.module.css";
 
-class SignUpForm extends Component {
+class Settings extends Component {
   render() {
     return (
       <div className={classes.mainContainer}>
         <div className={classes.headingContainer}>
-          <p className={classes.heading}>Sign Up</p>
+          <p className={classes.heading}>Settings</p>
         </div>
         <form className={classes.loginForm} action="/user" method="POST">
           <div
@@ -16,21 +16,7 @@ class SignUpForm extends Component {
             <input type="name" name="name" id="name"></input>
           </div>
           <div className={classes.subTextContainer}>
-            <p className={classes.subText}>
-              Note: Calendar will call you with this name. Choose whatever name
-              you like.
-            </p>
-          </div>
-          <div
-            className={`${classes.formGroupContainer} ${classes.marginBottom}`}
-          >
-            <label htmlFor="email">Email:</label>
-            <input type="email" name="email" id="email"></input>
-          </div>
-          <div className={classes.subTextContainer}>
-            <p className={classes.subText}>
-              Note: You will need to verify this. Make it real.
-            </p>
+            <p className={classes.subText}>I don't like my name either.</p>
           </div>
           <div
             className={`${classes.formGroupContainer} ${classes.marginBottom}`}
@@ -48,24 +34,10 @@ class SignUpForm extends Component {
               id="repeat-password"
             ></input>
           </div>
-          <div className={classes.subTextContainer}>
-            <div className={classes.subText}>
-              <input
-                type="checkbox"
-                name="dislike"
-                value="justforfun"
-                className={classes.checkboxInput}
-              />
-              <label htmlFor="dislike" className={classes.checkboxLabel}>
-                {" "}
-                I dislike filling forms. Use telepathy next time.{" "}
-              </label>
-            </div>
-          </div>
           <input type="hidden" name="_csrf" value="csrfToken" />
           <div className={classes.btnWrapper}>
             <button className={classes.btn} type="submit">
-              Sign Up
+              Save
             </button>
           </div>
         </form>
@@ -74,4 +46,4 @@ class SignUpForm extends Component {
   }
 }
 
-export default SignUpForm;
+export default Settings;
