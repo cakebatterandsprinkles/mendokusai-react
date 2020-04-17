fae51c3f2ef3ac341a15944e56e768f83f980b38
yagmurcetintas <yagmurcetin@gmail.com>
2020-04-17 11:57:44 -0400

    added footer that contains a legend to the user main page

--- src/components/NavbarUser/NavbarUser.jsx
+++ src/components/NavbarUser/NavbarUser.jsx
@@ -1,54 +1,66 @@
-import React from "react";
+import React, { Component } from "react";
 import downarrow from "../../assets/images/downarrow.png";
 import classes from "./NavbarUser.module.css";
 import NavbarInfoBox from "../NavbarInfoBox/NavbarInfoBox";
 import SettingsIcon from "../../assets/images/settingsicon.png";
 import { Link } from "react-router-dom";
 
-const NavbarUser = (props) => {
-  return (
-    <div className={classes.flexContainerRow}>
+class NavbarUser extends Component {
+  reverseColor = () => {
+    alert("reversed color!");
+  };
+
+  render() {
+    return (
       <div className={classes.flexContainerRow}>
-        <Link className={classes.logo} to="/user">
-          {" "}
-          Mendokusai{" "}
-        </Link>
-        <img src={downarrow} alt="down arrow" className={classes.arrow} />
-        <NavbarInfoBox name="Ari Sensei" />
-      </div>
-      <div className={classes.dropdown}>
-        <img
-          src={SettingsIcon}
-          alt="settings icon"
-          className={`${classes.settingsIcon} ${classes.dropbtn}`}
-        />
-        <div className={classes.dropdownContent}>
-          <div className={classes.linkContainer}>
-            <Link
-              to="/settings"
-              style={{
-                textDecoration: "none",
-                color: "black",
-              }}
-            >
-              Settings
-            </Link>
+        <div className={classes.flexContainerRow}>
+          <Link className={classes.logo} to="/user">
+            {" "}
+            Mendokusai{" "}
+          </Link>
+          <img src={downarrow} alt="down arrow" className={classes.arrow} />
+          <NavbarInfoBox name={"Ari Sensei"} />
+          <div className={classes.changeColorBtn} onClick={this.reverseColor}>
+            <p>☯</p>
           </div>
-          <div className={classes.linkContainerBottom}>
-            <Link
-              to="/"
-              style={{
-                textDecoration: "none",
-                color: "black",
-              }}
-            >
-              Logout
-            </Link>
+          <div className={classes.calendarBtn}>
+            <p>Calendar»</p>
+          </div>
+        </div>
+        <div className={classes.dropdown}>
+          <img
+            src={SettingsIcon}
+            alt="settings icon"
+            className={`${classes.settingsIcon} ${classes.dropbtn}`}
+          />
+          <div className={classes.dropdownContent}>
+            <div className={classes.linkContainer}>
+              <Link
+                to="/settings"
+                style={{
+                  textDecoration: "none",
+                  color: "black",
+                }}
+              >
+                Settings
+              </Link>
+            </div>
+            <div className={classes.linkContainerBottom}>
+              <Link
+                to="/"
+                style={{
+                  textDecoration: "none",
+                  color: "black",
+                }}
+              >
+                Logout
+              </Link>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default NavbarUser;
