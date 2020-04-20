d1c2161eda18c33db26ede0202128917d3f5e9a2
yagmurcetintas <yagmurcetin@gmail.com>
2020-04-20 11:19:58 -0400

    added footer dynamic rendering

--- src/components/LegendFooter/LegendFooter.jsx
+++ src/components/LegendFooter/LegendFooter.jsx
@@ -1,26 +1,62 @@
-import React from "react";
+import React, { Component } from "react";
 import classes from "./LegendFooter.module.css";
 import Donecheckbox from "../../assets/images/donecheckbox.png";
 import Notdonecheckbox from "../../assets/images/notdonecheckbox.png";
 import Inprogress from "../../assets/images/inprogresscheckbox.png";
+import Circle from "../../assets/images/circle.png";
+import Triangle from "../../assets/images/triangle.png";
+import Star from "../../assets/images/star.png";
 
-const LegendFooter = () => {
-  return (
-    <div className={classes.footer}>
-      <div className={classes.legendItem}>
-        <img src={Notdonecheckbox} alt="Not done checkbox" />
-        <p className={classes.footerText}>Not Done</p>
+class LegendFooter extends Component {
+  state = {
+    currentPath: "",
+  };
+
+  getCurrentPath = () => {
+    this.setState({ currentPath: window.location.pathname });
+  };
+
+  componentDidMount() {
+    this.getCurrentPath();
+  }
+
+  render() {
+    return (
+      <div className={classes.footer}>
+        <div className={classes.legendItem}>
+          <img
+            src={
+              this.state.currentPath.includes("calendar")
+                ? Circle
+                : Notdonecheckbox
+            }
+            alt="Not done checkbox"
+          />
+          <p className={classes.footerText}>Not Done</p>
+        </div>
+        <div className={classes.legendItem}>
+          <img
+            src={
+              this.state.currentPath.includes("calendar")
+                ? Triangle
+                : Inprogress
+            }
+            alt="In progress"
+          />
+          <p className={classes.footerText}>In Progress</p>
+        </div>
+        <div className={classes.legendItem}>
+          <img
+            src={
+              this.state.currentPath.includes("calendar") ? Star : Donecheckbox
+            }
+            alt="Done Checkbox"
+          />
+          <p className={classes.footerText}>Done</p>
+        </div>
       </div>
-      <div className={classes.legendItem}>
-        <img src={Inprogress} alt="In progress" />
-        <p className={classes.footerText}>In Progress</p>
-      </div>
-      <div className={classes.legendItem}>
-        <img src={Donecheckbox} alt="Done Checkbox" />
-        <p className={classes.footerText}>Done</p>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default LegendFooter;
