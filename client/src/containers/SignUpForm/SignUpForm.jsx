c8c7867dee65c8c05a38c369d00cbe140501bd62
yagmurcetintas <yagmurcetin@gmail.com>
2020-04-29 10:08:08 -0400

    Added auth controller

--- client/src/containers/SignUpForm/SignUpForm.jsx
+++ client/src/containers/SignUpForm/SignUpForm.jsx
@@ -21,7 +21,7 @@ class SignUpForm extends Component {
 
   submitForm = () => {
     axios
-      .post("/user", {
+      .post("/signup", {
         name: this.state.name,
         email: this.state.email,
         password: this.state.password,
@@ -29,8 +29,7 @@ class SignUpForm extends Component {
       })
       .then((response) => console.log(response))
       .catch((error) => {
-        const errors = error.response.data.errors;
-        console.log(errors);
+        console.log(error);
       });
   };
 
